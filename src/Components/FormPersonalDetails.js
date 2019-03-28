0da6472cec4d1ac13370364dd7adfbc1fd38ce47
Nicolas Kobal <nicolas.hacala@gmail.com>
2019-03-28 18:27:39 +0000

    eod 28/03

---
+++ src/Components/FormPersonalDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FormPersonalDetails extends Component {
+  render() {
+    return (
+      <div>
+        <h1>FormPersonalDetails</h1>
+      </div>
+    )
+  }
+}
+
+export default FormPersonalDetails
